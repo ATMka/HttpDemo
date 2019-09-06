3de29fb23d3768c30150a969cc1879d22c973aa2
atmka <corp.dvd@gmail.com>
2019-09-06 20:29:35 +0400

    Доработки

--- src/main/java/com/epam/extlab/Main.java
+++ src/main/java/com/epam/extlab/Main.java
@@ -1,9 +1,5 @@
 package com.epam.extlab;
 
-import kong.unirest.Header;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
-
 public class Main {
     public static void main(String[] args) {
         new DemoService().go();
